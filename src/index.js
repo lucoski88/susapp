42a6c1c9940baf514c2c8c25afbf70bedf91c41b
xcocco <marckciau@gmail.com>
2025-05-30 18:47:50 +0200

    Added database connection

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 const app = require('./app');
+const { connectDb, getDb } = require('./config/db');
+
 const port = 3000;
 
 /*
  * Start listening on the specified port
  */
-app.listen(port, () => {
-    console.log("Listening on port " + port);
-});
+async function startServer() {
+    /*
+     * blocking until database connection is established correctly or
+     * timeout for database connection occurs
+     */
+    await connectDb();
+    const db = getDb();
+    if (db) {
+        console.log('Database initialized');
+    } else {
+        console.log("Couldn't initialize database");
+        process.exit(1);
+    }
+
+    app.listen(port, () => {
+        console.log("Listening on port " + port);
+    });
+}
+
+startServer();

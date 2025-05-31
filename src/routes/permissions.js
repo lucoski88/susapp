a2cba90cc21b0834d0ba3f5c8936103321640de0
lucad <l.depasquale5@studenti.unisa.it>
2025-05-31 20:11:05 +0200

    Added endpoints

--- src/routes/permissions.js
+++ src/routes/permissions.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-
+router.get('/');
+router.post('/create');
+router.post('/update');
+router.post('/delete');
 
 module.exports = router;

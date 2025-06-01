21b97898a6434a3cc5524375c31a63bec0dadc5a
xcocco <marckciau@gmail.com>
2025-06-01 19:03:45 +0200

    Small fixes

--- src/controllers/permissionsController.js
+++ src/controllers/permissionsController.js
@@ -1,11 +1,7 @@
 const { getDb } = require('../config/db');
 
 exports.get = async (req, res, next) => {
-    try {
-        let db = getDb();
-        let collection = db.collection('permissions');
-        res.json(await collection.findOne());
-    } catch(err) {
-        return next(err);
-    }
+    let db = getDb();
+    let collection = db.collection('permissions');
+    res.json(await collection.findOne());
 };

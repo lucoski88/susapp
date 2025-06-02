3878b0850acc1b6c45014a71406c41291f06dd8c
lucad <l.depasquale5@studenti.unisa.it>
2025-06-02 15:44:15 +0200

    Added array schema in permissionModel.js

--- src/models/permissionModel.js
+++ src/models/permissionModel.js
@@ -2,10 +2,16 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const PermissionArraySchema = new Schema({
+    permission: String,
+    type: String
+}, { _id: false });
+
 const PermissionSchema = new Schema({
     _id: String,
     appId : String,
-    appName : String
+    appName : String,
+    allPermissions: [PermissionArraySchema]
 });
 
 module.exports = mongoose.model('Permission', PermissionSchema);

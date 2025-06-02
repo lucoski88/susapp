d9aa0bfb246d2ff1ed465bce9263df02090d589c
lucad <l.depasquale5@studenti.unisa.it>
2025-06-02 16:19:14 +0200

    Basic implementation of permissionsController.js

--- src/controllers/permissionsController.js
+++ src/controllers/permissionsController.js
@@ -1,6 +1,21 @@
 const Permission = require('../models/permissionModel')
 
+const defaultLimit = 10;
+const maxLimit = 100;
+
 exports.get = async (req, res, next) => {
-    const result = await Permission.findOne();
+    const { appId, appName } = req.query;
+    const filter = {};
+    if (appId) filter.appId = appId;
+    if (appName) filter.appName = appName;
+
+    let limit = req.query.limit;
+    if (limit) {
+        Math.min(maxLimit, limit);
+    } else {
+        limit = defaultLimit;
+    }
+
+    const result = await Permission.find(filter).limit(limit);
     res.json(result);
 };
